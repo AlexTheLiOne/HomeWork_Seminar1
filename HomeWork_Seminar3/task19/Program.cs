083d31cb8796b674c1e5ad045534f53bb231f767
AlexTheLiOne <alz4ur@yandex.ru>
2022-12-04 22:57:08 +0300

    HomeWork Seminar3

--- HomeWork_Seminar3/task19/Program.cs
+++ HomeWork_Seminar3/task19/Program.cs
@@ -3,4 +3,15 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine
+Console.WriteLine("Enter a 5-digit number to define a palindrome: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n / 1000;
+int i2 = n / 10000;
+if (i2 == n % 10 && i % 10 == (n / 10) % 10)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
