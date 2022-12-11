6460fa91f45fbd2080f12f5d980ddd8cb734dd58
AlexTheLiOne <alz4ur@yandex.ru>
2022-12-11 22:13:43 +0300

    HomeWork4

--- HomeWork_Seminar4/task29/Program.cs
+++ HomeWork_Seminar4/task29/Program.cs
@@ -1,8 +1,12 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
 
+
+
 int[] array = new int[8];
 for (int i = 0; i < 8; i++)
 {
-    Console.WriteLine(i);
+    Console.WriteLine("Enter a number: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    array [i] = n;
 }
 Console.WriteLine($"[{string.Join(", ", array)}]");
