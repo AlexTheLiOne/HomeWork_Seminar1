9c36003d065d476615462b4791f32e692962eae4
AlexTheLiOne <alz4ur@yandex.ru>
2022-12-17 09:26:20 +0300

    HomeWork

---
+++ Seminar6/task40/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Enter 3 numbers to check if a triangle exists");
+
+Console.WriteLine("Enter a number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a number 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a < b + c && b < a + c && c < a + b) Console.WriteLine("The triangle exists");
+
+else
+{
+    Console.WriteLine("The triangle doesn't exist");
+}
---
+++ Seminar6/task42/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Enter a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int y = 0;
+
+for (int i = 0; i < n; i++)
+{
+    
+}
