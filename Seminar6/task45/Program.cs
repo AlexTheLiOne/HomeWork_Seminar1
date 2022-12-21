d3f3894ecfe5f0ed2f89045f773249aed2374b07
AlexTheLiOne <alz4ur@yandex.ru>
2022-12-21 18:22:13 +0300

    HomeWork

--- Seminar6/task45/Program.cs
+++ Seminar6/task45/Program.cs
@@ -1,2 +1,10 @@
 ﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования
 
+int[] array = {52, 43, 59, 73, 35, 74};
+int[] arr = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    arr[i] = array[i];
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", arr)}]");
---
+++ T/massivePandN/Affort/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter an array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine("Enter a number: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    array[i] = n;
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+int[] arr = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    arr[i] = array[i];
+}
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+
