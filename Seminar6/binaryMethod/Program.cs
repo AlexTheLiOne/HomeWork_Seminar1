4d82b48160f9363b65a95eae6c021f2112580ad3
AlexTheLiOne <alz4ur@yandex.ru>
2022-12-21 17:51:42 +0300

    HomeWork

---
+++ Seminar6/binaryMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿// Method to show number binary view
+
+int a = 13;
+
+void BinaryMethod (int number)
+{
+    if (number <= 0) return;
+    BinaryMethod (number / 2);
+    Console.Write(number % 2);
+}
+BinaryMethod(a);
--- Seminar6/task42/Program.cs
+++ Seminar6/task42/Program.cs
@@ -3,12 +3,41 @@
 // 3 -> 11
 // 2 -> 10
 
+Console.Clear();
 Console.WriteLine("Enter a number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int y = 0;
-
-for (int i = 0; i < n; i++)
+int Method1(int numb)
 {
-    
+   int i = 0;
+   while (numb > 0)
+   {
+      numb = numb / 2;
+      i++;
+   }
+   return i;
 }
+
+int[] array = new int[Method1(n)];
+int b = array.Length - 1;
+while (n > 0)
+{
+   array[b] = n % 2;
+   n = n / 2;
+   b--;
+}
+Console.WriteLine(string.Join("", array));
+// int div = 0;
+// int mult = 0;
+// int nul = 0;
+// int i = 0;
+// int[] array = new int{i};
+// while (i < n)
+// {
+//    div = n / 2;
+//    mult = div * 2;
+//    nul = n - mult;
+//    n = div;
+//    i++;
+//    Console.WriteLine(array[i] = nul);
+// }
