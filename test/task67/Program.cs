b732eff6ab870992fa50ecafa9aa65f3a95b3b2a
AlexTheLiOne <alz4ur@yandex.ru>
2023-01-09 06:50:32 +0300

    HomeWork

--- test/task67/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
-// 453 -> 12
-// 45 -> 9
-
-Console.WriteLine("Enter a number: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
